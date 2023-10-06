07bb0d96ea59bd93abdb6362f120e3fc10c39410
EvertonNdiawo <ndiawofreelancer@gmail.com>
2023-10-06 01:08:28 -0700

    Create new file 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+/**
+ * main - A program to print text on screen
+ *
+ * Description: This program prints hello world
+ * on the screen without using the printf and
+ * puts functions.
+ *
+ * Return: always return 1(success)
+ */
+int main(void)
+{
+	system(" echo 'Hello, World' ");
+	return (1);
+}
