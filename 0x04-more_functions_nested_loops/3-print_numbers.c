747e264c7df8ad5e24fe5bc55d82731ee048135d
EvertonNdiawo <ndiawofreelancer@gmail.com>
2023-10-13 14:55:48 -0700

    create file 3-print_numbers.c, modify main.h

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers from 0 to 9
+ *
+ * return: returns 1(success)
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(i);
+	}
+
+	-putchar('\n');
+}
