0b627b9cc87a24f3c20325c9570034abf5f2fd61
EvertonNdiawo <ndiawofreelancer@gmail.com>
2023-10-13 16:16:08 -0700

    create file 5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 0 t0 14 ten times,folled by a newline
+ */
+void more_numbers(void)
+{
+	for (int i = 0; i < 10; i++)
+	{
+		for (int j = 0; j <= 14; j++)
+		{
+			char str[15];
+
+			_putchar(str[j]);
+		}
+
+		_putchar('\n');
+	}
+}
