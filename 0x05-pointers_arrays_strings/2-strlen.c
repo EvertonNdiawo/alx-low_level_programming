b7d2cf82600be8494ec9d2b4edaa53c2dbff54e8
EvertonNdiawo <ndiawofreelancer@gmail.com>
2023-10-16 13:32:26 -0700

    create c file 2-strlent.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string.
+ *
+ * @s: pointer to the string to be checked.
+ *
+ * Return: returns length of a string passed to it.
+ */
+int _strlen(char *s)
+{
+	int i;
+	int counter;
+
+	counter = 0;
+
+	i = s[0];
+
+	while (i != '\0')
+	{
+		counter++;
+	}
+
+	return (counter);
+}
