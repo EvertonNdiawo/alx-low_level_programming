380001340868f8c06ae98644d2fdb702835b4c36
EvertonNdiawo <ndiawofreelancer@gmail.com>
2023-10-17 12:02:55 -0700

    create c file 5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ *
+ * @s: string argument/parameter to be reversed
+ *
+ * Return: nothing
+ */
+void rev_string(char *s)
+{
+	int i = 0;
+	int count;
+
+	while (s[count] != '\0')
+	{
+		count++;
+	}
+
+	for (i = count - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+
+	_putchar('\n');
+}
