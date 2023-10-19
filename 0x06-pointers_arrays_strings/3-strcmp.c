253c5bb66a75b39f75d551a015f8d4d0cecf800b
EvertonNdiawo <ndiawofreelancer@gmail.com>
2023-10-19 16:00:02 -0700

    Create c file 3-strcmp.c and a main file 3-main.c to test it.

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings.
+ *
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: returns an int less than 0 if string is less than string
+ * compared against, 0 if strings equal and int greater than 0 if sting
+ * is greater than other string.
+ *
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int s1len = 0;
+	int s2len = 0;
+
+	while (s1[s1len] != '\0')
+	{
+		s1len++;
+	}
+
+	while (s2[s2len] != '\0')
+	{
+		s2len++;
+	}
+
+	if (s1len < s2len)
+	{
+		return (10);
+	}
+	else if (s1len == s2len)
+	{
+		return (0);
+	}
+	else
+	{
+		return (-10);
+	}
+}
