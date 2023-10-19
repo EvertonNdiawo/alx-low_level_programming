b024b459441fadeef2f2b7cdfe19b1916cf3a698
EvertonNdiawo <ndiawofreelancer@gmail.com>
2023-10-19 06:43:15 -0700

    modify 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -23,12 +23,12 @@ char *_strcat(char *dest, char *src)
 
 	while (src[len2] != '\0')
 	{
-		dest[len1 + 1] = src[len2];
+		dest[len1] = src[len2];
 		len1++;
 		len2++;
 	}
 
-	dest[len1 + 1] = '\0';
+	dest[len1] = '\0';
 
 	return (dest);
 }
