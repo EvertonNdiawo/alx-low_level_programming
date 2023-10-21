8eec71f92591b504ce72e749902234e849b4abc0
EvertonNdiawo <ndiawofreelancer@gmail.com>
2023-10-20 19:12:58 -0700

    Modify 3-strcmp.c to write the code in as simple terms as possible

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -14,33 +14,16 @@
 int _strcmp(char *s1, char *s2)
 {
 
-	int i = 0;
-
-	while (s1[i] != '\0' && s2[i] != '\0')
+	while (*s1 == *s2)
 	{
-		if (s1[i] > s2[i])
+		if (*s1 == '\0')
 		{
-			return (1);
-		}
-		else if (s1[i] < s2[i])
-		{
-			return (-1);
+			return (0);
 		}
 
-		i++;
+		s1++;
+		s2++;
 	}
 
-	if (s1[i] == '\0' && s2[i] == '\0')
-	{
-		return (0);
-	}
-	else if (s1[i] == '\0')
-	{
-		return (-1);
-	}
-	else
-	{
-		return (1);
-	}
-
+	return (*s1 - *s2);
 }
