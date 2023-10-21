0e2ebd0c2211da398496d3ef87823c7ca0a8480f
EvertonNdiawo <ndiawofreelancer@gmail.com>
2023-10-20 19:01:10 -0700

    modify

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -26,11 +26,21 @@ int _strcmp(char *s1, char *s2)
 		{
 			return (-1);
 		}
-		else
-		{
-			return (0);
-		}
 
 		i++;
 	}
+
+	if (s1[i] == '\0' && s2[i] == '\0')
+	{
+		return (0);
+	}
+	else if (s1[i] == '\0')
+	{
+		return (-1);
+	}
+	else
+	{
+		return (1);
+	}
+
 }
