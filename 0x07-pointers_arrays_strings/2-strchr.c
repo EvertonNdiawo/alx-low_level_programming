1d68b1239a93934efcc595c0eb263b54034f6d0a
EvertonNdiawo <ndiawofreelancer@gmail.com>
2023-10-24 22:55:48 +0300

    Create c file 2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#define NULL ((void *)0)
+
+/**
+ * _strchr - Locates a character in a string aand returns a pointer to
+ * the first occurrence of the character in the string, or NULL if
+ * the character is not found.
+ *
+ * @s: The string to be traversed.
+ * @c: character to be found.
+ *
+ * Return: return pointer to first occurence of c or NULL if
+ * character c is not found.
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+	int len = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+
+	for (i = 0; i < len; i++)
+	{
+		if (s[i] == c)
+		{
+			return (&s[i]);
+		}
+	}
+
+	return (NULL);
+}
