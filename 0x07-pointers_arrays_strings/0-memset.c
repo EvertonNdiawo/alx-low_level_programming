e1a7e50ff7ba3a89db178f359d7dcdb2f4ea47e9
EvertonNdiawo <ndiawofreelancer@gmail.com>
2023-10-24 16:30:23 +0300

    Create files 0-main.c 0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _memset - fills memory with a constant byte.
+ *
+ * @s: pointer to where the function starts filling memory.
+ * @b: The character/byte to be filled in.
+ * @n: number of bytes to fill.
+ *
+ * Return: returns pointer to s.
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	while (i < n)
+	{
+		s[i] = b;
+		i++;
+	}
+
+	return (s);
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,4 +1,4 @@
-_putchar(char);
+char _putchar(char);
 
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
