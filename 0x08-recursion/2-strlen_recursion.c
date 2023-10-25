6ea424605740d1f0c351c1888e515e8119d3912f
EvertonNdiawo <ndiawofreelancer@gmail.com>
2023-10-26 00:33:26 +0300

    create c file 2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - Returns the length of a string.
+ *
+ * @s: pointer to the string whose length is to be returned.
+ *
+ * Return: returns the length of a string.
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+
+	return (1 + _strlen_recursion(s + 1));
+}
