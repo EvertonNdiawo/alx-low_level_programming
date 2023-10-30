474d4d379175283e8895d4727aec70feacfcc20f
EvertonNdiawo <ndiawofreelancer@gmail.com>
2023-10-31 00:28:56 +0300

    create files 1-main.c, 1-strdup.c and 1-strdup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - Returns a pointer to newly allocated space in memory
+ * that containsa copy of string given as a parameter.
+ *
+ * @str: string whose copy is to be returned as a pointer.
+ *
+ * Return: pointer to new string that is a duplicate of string str (success)
+ * Null otherwise.
+ *
+ */
+
+char *_strdup(char *str)
+{
+	int i = 0;
+	int len = 0;
+	char *string;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	while (str[len])
+	{
+		len++;
+	}
+
+	string = malloc(sizeof(char) * (len + 1));
+
+	while (i < len)
+	{
+		string[i] = str[i];
+		i++;;
+	}
+	
+	string[i] = '\0';
+
+	if (string == NULL)
+	{
+		return (NULL);
+	}
+
+	return (string);
+
+}
