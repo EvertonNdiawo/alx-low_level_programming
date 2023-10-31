3a6f6e1bf9e12536ed05b1ec8ca662af03f98590
EvertonNdiawo <ndiawofreelancer@gmail.com>
2023-10-31 22:55:26 +0300

    update 3-alloc_grid to correct betty styling

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -16,24 +16,19 @@ int **alloc_grid(int width, int height)
 	int i, j;
 
 	if (width <= 0 || height <= 0)
-	{
 		return (NULL);
-	}
 
 	arr = malloc(height * sizeof(int *));
 
 	if (arr == NULL)
-	{
 		return (NULL);
-	}
-
 	i = 0;
 	j = 0;
 
 	while (i < height)
 	{
 		arr[i] = malloc(width * sizeof(int));
-		
+
 		if (arr[i] == NULL)
 		{
 			while (i >= 0)
@@ -41,7 +36,6 @@ int **alloc_grid(int width, int height)
 				free(arr[i]);
 				i--;
 			}
-
 			free(arr);
 			return (NULL);
 		}
