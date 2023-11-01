0d3fd56790786dd63f0c088ae7311c47168214dc
EvertonNdiawo <ndiawofreelancer@gmail.com>
2023-11-01 18:53:23 +0300

    corrected main.c to ensure compliance with betty coding style

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,7 +1,14 @@
 #include "main.h"
 
+/**
+ * main - uses static library to make use of a funtion's object code in it.
+ *
+ * Return: 0(success)
+ */
+
 int main(void)
 {
-    _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
-    return (0);
+	_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t
+			- Kevin Mitnick");
+	return (0);
 }
--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,4 +1,8 @@
+#ifndef MAIN_FILE
+#define MAIN_FILE
+
 int _putchar(char c);
+
 int _islower(int c);
 int _isalpha(int c);
 int _abs(int n);
@@ -18,3 +22,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+
+#endif
