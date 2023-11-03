9d486930eca8b850ce79c6bbf89740136657ada5
EvertonNdiawo <ndiawofreelancer@gmail.com>
2023-11-03 17:21:05 +0300

    create file 2-calloc.c that implements the calloc function in the standard c library

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - allocates memory for an array, using malloc
+ *
+ * @nmemb: number of array elements
+ * @size: size of nmemb elements
+ *
+ * Return: pointer to the allocated memory location(success)
+ * otherwise returns NULL(failure)
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *ptr;
+	
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+
+	ptr = malloc(sizeof(size) * nmemb);
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+
+	return (ptr);
+}
