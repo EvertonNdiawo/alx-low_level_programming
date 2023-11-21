a2555d9ce4e938017e5318d5ee9aa2532316a41f
EvertonNdiawo <ndiawofreelancer@gmail.com>
2023-11-21 17:47:38 +0300

    I dont understand what the hell I just did, but I have to stay in the program

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,16 +1,12 @@
 global main
 extern printf
 
-
-section .data 
-	msg db 'Hello, Holberton', 0
-
-section .text
-
 main:
 
-	mov edi, msg
+	mov edi, format
 	xor eax, eax
 	call printf
 	mov eax, 0
 	ret
+
+format: db'Hello, Holberton\n',0
