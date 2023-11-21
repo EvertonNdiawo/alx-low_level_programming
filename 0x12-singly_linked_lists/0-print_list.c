1de3ebf6abcf1d2c51019e3ac27dbd51946ac383
EvertonNdiawo <ndiawofreelancer@gmail.com>
2023-11-21 03:18:49 +0300

    fixed indefinite loop in print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -14,15 +14,18 @@ size_t print_list(const list_t *h)
 {
 	size_t counter = 0;
 
-	if (h->str == NULL)
-	{
-		printf("[0] (nil)\n");
-	}
-
-
 	while (h)
 	{
-		printf("[%u] %s\n", h->len, h->str);
+		if (!h->str)
+		{
+			printf("[0] (nil)\n");
+		}
+
+		else
+		{
+			printf("[%u] %s\n", h->len, h->str);
+		}
+
 		h = h->next;
 
 		counter++;
