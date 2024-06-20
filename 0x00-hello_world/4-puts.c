7b6bd747b741969b334be4bb2ef243f83548adb6
EvertonNdiawo <ndiawofreelancer@gmail.com>
2024-06-20 14:45:20 -0700

    Create file 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Where execution begins
+ *
+ * Description: A program that prints to the standard out
+ * Return:0 succes, -1 fail.
+ */
+
+int main(void)
+{
+	char *str = "Programming is like building a multilingual puzzle\n";
+
+	puts(str);
+	return (0);
+}
