d2c04af0cec238de49295aaa6f4253ff249f7219
EvertonNdiawo <ndiawofreelancer@gmail.com>
2024-06-21 13:56:23 -0700

    create file 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include<unistd.h>
+#include<string.h>
+
+/**
+ * main - entry point of code
+ *
+ * Return: 1 always a success.
+ */
+
+int main(void)
+{
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	int length = strlen(str);
+
+	write(2, str, length);
+}
