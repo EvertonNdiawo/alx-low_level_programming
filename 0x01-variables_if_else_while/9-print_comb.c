cc96efc5daa8327e2c0604cd037f4f85b4570c0c
EvertonNdiawo <ndiawofreelancer@gmail.com>
2024-06-26 03:54:44 -0700

    Modify file 9-print_comb.c and 9-print_comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,17 +10,14 @@ int main(void)
 {
 	int i;
 
-	for (i = 48; i < 58; i++)
+	for (i = 48; i <= 57; i++)
 	{
+		putchar(i);
+
 		if (i != 57)
 		{
-			putchar(i);
 			putchar(',');
 			putchar(' ');
-		}
-		else
-		{
-			putchar(i);
 		}
 	}
 
