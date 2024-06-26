8144256f33acf8683042d54709c949acf44a891b
EvertonNdiawo <ndiawofreelancer@gmail.com>
2024-06-26 01:52:05 -0700

    Create files 5-print_numbers.c and 5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point of code.
+ *
+ * Return: 0 always a success.
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+
+	putchar('\n');
+	return (0);
+}
+
