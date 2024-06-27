f7351823ee795b81a19de9588fde0cba9f217dee
EvertonNdiawo <ndiawofreelancer@gmail.com>
2024-06-27 12:28:16 -0700

    Create file 0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include <main.h>
+
+/**
+ * main - a program that tests a different program
+ *
+ * Return: 0, success.
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	positive_or_negative(i);
+
+	return (0);
+}
