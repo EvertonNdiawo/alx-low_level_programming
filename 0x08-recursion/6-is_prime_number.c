77eaf6fd6eb7b94ba7c521a22e63271cea96e8ca
EvertonNdiawo <ndiawofreelancer@gmail.com>
2024-07-11 17:00:05 -0700

    Modify 6-is_prime_number.c to correct documentation issues.

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,10 +1,13 @@
 #include "main.h"
 
 /**
- * is_prime-helper - Checks for the divisibility of a number.
+ * prime-helper - Checks for the divisibility of a number.
  * @n: The number to check for divisibility.
  * @i: the divisor.
  *
+ * Description: Does a divisibility test on parameter n starting with i
+ * and procedes to return the result whether parameter n is a prime
+ * number or not.
  * Return: 1(n has no divisor), 0(i is a successful divisor).
  */
 
