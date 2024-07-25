8f6b3ed993c8a27af77e16bf3e4237f77ff0d143
EvertonNdiawo <ndiawofreelancer@gmail.com>
2024-07-25 20:03:47 +0300

    create file 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguments passed to it.
+ *
+ * @argc: parameter that takes care of number of arguments.
+ * @argv: array of actual arguments passed.
+ *
+ * Return: 0(success).
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+}
