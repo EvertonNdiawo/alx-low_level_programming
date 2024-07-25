e7ce788e22055e29ff9a8437034994660493bd46
EvertonNdiawo <ndiawofreelancer@gmail.com>
2024-07-25 20:31:24 +0300

    correct logic in 1-args.c to exclude the program name from the number of actual arguments.

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - prints the number of arguments passed to it.
+ * main - prints the number of arguments passed into it.
  *
  * @argc: parameter that takes care of number of arguments.
  * @argv: array of actual arguments passed.
@@ -11,7 +11,7 @@
 
 int main(int argc, char *argv[] __attribute__((unused)))
 {
-	printf("%d\n", argc);
+	printf("%d\n", argc-1);
 
 
 	return (0);
