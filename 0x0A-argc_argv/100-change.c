28f139511bca989e7db0bb9d36f75da1aa6b6ef0
EvertonNdiawo <ndiawofreelancer@gmail.com>
2024-08-02 12:27:42 +0300

    Refactor 100-change.c code to comply with betty documentation rules

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -8,50 +8,34 @@
  *
  * Return: the minimum number of coins(success), 1(Error), 0(failure).
  */
+
 int main(int argc, char **argv)
 {
 	int total_coins = 0;
 	int cents;
+	int denominations[] = {25, 10, 5, 2, 1};
+	int i;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
+
 	cents = atoi(argv[1]);
+
 	if (cents < 0)
 	{
 		printf("0\n");
 		return (0);
 	}
-	while (cents > 0)
+
+	for (i = 0; i < 5 && cents > 0; i++)
 	{
-		if (cents >= 25)
-		{
-			total_coins += cents / 25;
-			cents = cents % 25;
-		}
-		else if (cents >= 10)
-		{
-			total_coins += cents / 10;
-			cents = cents % 10;
-		}
-		else if (cents >= 5)
-		{
-			total_coins += cents / 5;
-			cents = cents % 5;
-		}
-		else if (cents >= 2)
-		{
-			total_coins += cents / 2;
-			cents = cents % 2;
-		}
-		else if (cents >= 1)
-		{
-			total_coins += cents / 1;
-			cents = cents % 1;
-		}
+		total_coins += cents / denominations[i];
+		cents %= denominations[i];
 	}
+
 	printf("%d\n", total_coins);
 	return (0);
 }
