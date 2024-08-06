ebc1942b558717b7dc06e31a811cdb67f27e5ace
EvertonNdiawo <ndiawofreelancer@gmail.com>
2024-08-06 19:10:55 +0300

    Add description documentation for the structure declared within the dog.h file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,16 @@
 #ifndef _DOG_
 #define _DOG_
 
+/**
+ * struct dog - contains details of a dog.
+ * @name: pointer to the name of the dog.
+ * @age: The age of the dog.
+ * @owner: pointer to the owner of the dog.
+ *
+ * Description: A structure to store the details and descriptions of a dog
+ * its name, age and the owner.
+ */
+
 struct dog
 {
 	char *name;
