d35b9b6545f2b887cd263ebf638d84a66d2a4f43
EvertonNdiawo <ndiawofreelancer@gmail.com>
2024-08-09 20:29:08 +0300

    Create file 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,5 +11,24 @@
 
 size_t print_list(const list_t *h)
 {
-	printf();
+	size_t num_elements;
+
+	num_elements = 0;
+
+	while ( h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[%d] %s\n", 0, "(nil)");
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		h = h->next;
+		num_elements++;
+	}
+
+	return (num_elements);
+
 }
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,6 @@
 #ifndef _LISTS_
 #define _LISTS_
+#include <stddef.h>
 
 /**
  * struct list_s - singly linked list
